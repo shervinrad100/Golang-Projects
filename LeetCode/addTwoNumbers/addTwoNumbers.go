e91d63ae4e9f6a6b4b292b579ce55b78a08eac3c
Shervin Rad <40181828+shervinrad100@users.noreply.github.com>
2022-03-21 19:30:37 +0000

    addTwoNumbers fixed

--- LeetCode/addTwoNumbers/addTwoNumbers.go
+++ LeetCode/addTwoNumbers/addTwoNumbers.go
@@ -1,3 +1,7 @@
+// stats:
+// Runtime: 4 ms, faster than 97.65%
+// Memory: 	4.7 MB, less than 47.01%
+
 package main
 
 import (
@@ -34,9 +38,15 @@ func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 		if sum > 9 {
 			sum -= 10
 			carry = 1
+		} else {
+			carry = 0
 		}
 		ansPointer.Val = sum
-		if (l1Pointer.Next == nil) && (l2Pointer.Next == nil) {
+		if (l1Pointer.Next == nil) && (l2Pointer.Next == nil) && (carry == 0) {
+			break
+		} else if (l1Pointer.Next == nil) && (l2Pointer.Next == nil) && (carry != 0) {
+			ansPointer.Next = &ListNode{Val: carry}
+			carry = 0
 			break
 		} else if (l1Pointer.Next != nil) && (l2Pointer.Next == nil) {
 			l1Pointer = l1Pointer.Next
@@ -54,21 +64,27 @@ func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 			ansPointer.Next = &ListNode{}
 			ansPointer = ansPointer.Next
 		}
+
 	}
+
 	return &ansHead
 }
 
 func main() {
 	l1 := ListNode{
-		Val:  11,
-		Next: &ListNode{Val: 12},
+		Val: 2,
+		Next: &ListNode{Val: 4,
+			Next: &ListNode{Val: 3},
+		},
 	}
 	l2 := ListNode{
-		Val:  21,
-		Next: &ListNode{Val: 22},
+		Val: 5,
+		Next: &ListNode{Val: 6,
+			Next: &ListNode{Val: 4},
+		},
 	}
 
 	output := AddTwoNumbers(&l1, &l2)
-	fmt.Println(output)
 	fmt.Println(SliceFromListNode(output))
+
 }
