462dc7ca5542f8d00e7eb9f4a2ca2b25f38126b1
Shervin Rad <shervin.sharifirad@team.bumble.com>
2026-09-02 23:41:39 +0100

    add logging

--- Ardan labs/tokenizeApi/main.go
+++ Ardan labs/tokenizeApi/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log/slog"
 	"net/http"
 	"os"
 	"regexp"
@@ -11,8 +12,12 @@ import (
 )
 
 func main() {
+	api := API{
+		log: slog.Default().With("component", "HTTP server"),
+	}
+
 	// Route
-	http.HandleFunc("POST /tokenize", tokenizeHandler)
+	http.HandleFunc("POST /tokenize", api.tokenizeHandler)
 
 	// Start server
 	if err := http.ListenAndServe(":3000", nil); err != nil {
@@ -21,42 +26,39 @@ func main() {
 	}
 }
 
-func tokenizeHandler(w http.ResponseWriter, r *http.Request) {
+type API struct {
+	log *slog.Logger
+}
+
+func (l *API) tokenizeHandler(w http.ResponseWriter, r *http.Request) {
 	// get the body from request
 	r.Body = http.MaxBytesReader(w, r.Body, 1024*1024) // 1MB
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		http.Error(w, "Failed to read body", http.StatusBadRequest)
+		l.log.Warn("Payload too large", "remote_addr", r.RemoteAddr, "", err)
+		http.Error(w, "Bad request", http.StatusBadRequest)
 		return
 	}
 	if !utf8.Valid(body) { // protect against XSS
-		http.Error(w, "Invalid UTF-8 encoded text", http.StatusBadRequest)
+		l.log.Info("Invalid UTF-8 encoded text:", "remote_addr", r.RemoteAddr, ":", err)
+		http.Error(w, "Bad request", http.StatusBadRequest)
 		return
 	}
 	defer r.Body.Close()
 
 	// tokenize
-	tokens, err := tokenize(body)
-	if err != nil {
-		http.Error(w, "Tokenizer failed", http.StatusInternalServerError)
-		return
+	res := map[string][]string{
+		"tokens": l.tokenize(body),
 	}
-
-	type TokenResponse struct {
-		Tokens []string `json:"tokens"`
-	}
-	res := TokenResponse{Tokens: tokens}
 
 	// marshalling
 	if err := json.NewEncoder(w).Encode(res); err != nil {
-		http.Error(w, "Failed to marshal JSON", http.StatusInternalServerError)
+		l.log.Error("Failed to marshal JSON", "", err)
+		http.Error(w, "Server error", http.StatusInternalServerError)
 		return
 	}
 }
 
-func tokenize(s []byte) ([]string, error) {
-	re := regexp.MustCompile(`[a-zA-z]+`)
-	tokens := re.FindAllString(string(s), -1)
-	// TODO error handling?
-	return tokens, nil
+func (l *API) tokenize(s []byte) []string {
+	return regexp.MustCompile(`[a-zA-z]+`).FindAllString(string(s), -1)
 }
