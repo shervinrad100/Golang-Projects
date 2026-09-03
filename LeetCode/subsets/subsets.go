627ce56d54b6e47271797bf80a022f6435e60f95
Shervin Rad <shervin.sharifirad@team.bumble.com>
2026-09-03 22:08:04 +0100

    subsets recursive

--- LeetCode/subsets/subsets.go
+++ LeetCode/subsets/subsets.go
@@ -3,8 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(subsets([]int{1, 2, 3}))
-	fmt.Println(subsets([]int{0}))
+	fmt.Println(subsetsRecursive([]int{1, 2, 3}))
+	fmt.Println(subsetsRecursive([]int{0}))
+	// fmt.Println(subsets([]int{1, 2, 3}))
+	// fmt.Println(subsets([]int{0}))
 }
 
 func subsets(nums []int) []string {
@@ -21,4 +23,29 @@ func subsets(nums []int) []string {
 		keys = append(keys, k)
 	}
 	return keys
+}
+
+func subsetsRecursive(nums []int) any {
+	ans, sub := [][]int{}, []int{}
+
+	var backtrack func(i int)
+	backtrack = func(i int) {
+		if i == len(nums) {
+			// ans = append(ans, sub[:]) WRONG!! Why?
+			temp := make([]int, len(sub))
+			copy(temp, sub)
+			ans = append(ans, temp)
+			return
+		}
+
+		// decision 1: dont pick
+		backtrack(i + 1)
+
+		// decision 2: pick num
+		sub = append(sub, nums[i])
+		backtrack(i + 1)
+		sub = sub[:len(sub)-1]
+	}
+	backtrack(0)
+	return ans
 }
